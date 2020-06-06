8f45b95d03861afdfe4e889d43a49e7731e6e237
Sergey Poznyakoff <gray@gnu.org>
2020-06-06 22:06:33 +0300

    Version 4.2.90

--- src/help.c
+++ src/help.c
@@ -2,7 +2,7 @@
    help.c
 
    This file is part of GNU Anubis.
-   Copyright (C) 2001-2014 The Anubis Team.
+   Copyright (C) 2001-2020 The Anubis Team.
 
    GNU Anubis is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
